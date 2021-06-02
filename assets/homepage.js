2be480c4bc2c38681b704ff882a29f8e0f8fbe93
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-06-02 16:37:18 -0700

    done with feature

--- assets/homepage.js
+++ assets/homepage.js
@@ -20,6 +20,7 @@ var formSubmitHandler = function(event) {
     alert("Please enter a GitHub username");
   }
 };
+
 var getUserRepos = function(user) {
   // format the github api url
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -53,14 +54,14 @@ var displayRepos = function(repos, searchTerm) {
   repoSearchTerm.textContent = searchTerm;
 
   // loop over repos
-for (var i = 0; i < repos.length; i++) {
-  // format repo name
-  var repoName = repos[i].owner.login + "/" + repos[i].name;
+  for (var i = 0; i < repos.length; i++) {
+    // format repo name
+    var repoName = repos[i].owner.login + "/" + repos[i].name;
 
-  // create a container for each repo
-  var repoEl = document.createElement("a");
-  repoEl.classList = "list-item flex-row justify-space-between align-center";
-  repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
+    // create a link for each repo
+    var repoEl = document.createElement("a");
+    repoEl.classList = "list-item flex-row justify-space-between align-center";
+    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
     // create a span element to hold repository name
     var titleEl = document.createElement("span");
--- assets/single.js
+++ assets/single.js
@@ -1,10 +1,24 @@
 var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
-var repoNameEl = document.querySelector("#repo-name");
 
+var getRepoName = function() {
+  // grab repo name from url query string
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
 
-var getRepoIssues = function(repo, repoName) {
+  if (repoName) {
+    // display repo name on the page
+    repoNameEl.textContent = repoName;
+
+    getRepoIssues(repoName);
+  } else {
+    // if no repo was given, redirect to the homepage
+    document.location.replace("./index.html");
+  }
+};
+
+var getRepoIssues = function(repo) {
   // format the github api url
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
@@ -20,28 +34,12 @@ var getRepoIssues = function(repo, repoName) {
           displayWarning(repo);
         }
       });
-    }
-    else {
-      console.log(response);
-      alert("There was a problem with your request!");
+    } else {
+      // if not successful, redirect to homepage
+      document.location.replace("./index.html");
     }
   });
 };
-
-var getRepoName = function() {
-    // grab repo name from url query string
-    var queryString = document.location.search;
-    var repoName = queryString.split("=")[1];
-
-    if(repoName) {
-        // display repo name on the page
-        repoNameEl.textContent = repoName;
-        getRepoIssues(repoName);
-      } else {
-        //   if no repo was given redirect to homepage
-          document.location.replace("./index.html");
-      }
-}
 
 var displayIssues = function(issues) {
   if (issues.length === 0) {
@@ -56,12 +54,11 @@ var displayIssues = function(issues) {
     issueEl.classList = "list-item flex-row justify-space-between align-center";
     issueEl.setAttribute("href", issues[i].html_url);
     issueEl.setAttribute("target", "_blank");
-    
+
     // create span to hold issue title
     var titleEl = document.createElement("span");
     titleEl.textContent = issues[i].title;
-    
-  
+
     // append to container
     issueEl.appendChild(titleEl);
 
@@ -71,14 +68,13 @@ var displayIssues = function(issues) {
     // check if issue is an actual issue or a pull request
     if (issues[i].pull_request) {
       typeEl.textContent = "(Pull request)";
-    }
-    else {
+    } else {
       typeEl.textContent = "(Issue)";
     }
 
     // append to container
     issueEl.appendChild(typeEl);
-  
+
     // append to the dom
     issueContainerEl.appendChild(issueEl);
   }
